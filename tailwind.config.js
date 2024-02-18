8c56964f109460faef410c103be651d1a3890002
ngyixuan <ngyixuan2009@gmail.com>
2024-02-18 15:20:59 +0800

    feat: implement about, timeline and projects

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}", "./pages/**/*.{html,js}"],
+  theme: {
+    fontSize: {
+      xxs: "0.5rem",
+      xs: "0.75rem",
+      sm: "0.938rem",
+      base: "1.125rem",
+      xl: "1.25rem",
+      "2xl": "1.5rem",
+      "3xl": "1.85rem",
+    },
+    extend: {
+      colors: {
+        purple: {
+          500: "#8468F5",
+        },
+        pink: {
+          500: "#EC74E7",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
