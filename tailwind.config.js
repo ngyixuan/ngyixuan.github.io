bac6a34c8e2135c9ebb926fdf81e86bedb639c33
ngyixuan <ngyixuan2009@gmail.com>
2024-02-22 20:27:11 +0800

    feat: add animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,7 @@ module.exports = {
       xl: "1.25rem",
       "2xl": "1.5rem",
       "3xl": "1.85rem",
-      "4xl": "2rem",
+      "4xl": "2.5rem",
       "5xl": "4rem",
     },
     extend: {
@@ -20,6 +20,59 @@ module.exports = {
         },
         pink: {
           500: "#EC74E7",
+        },
+      },
+
+      animation: {
+        "slide-in-right": "slideInRight 1.5s  ease-in-out",
+        "slide-in-left": "slideInLeft 1.5s  ease-in-out",
+        "slide-in-bottom": "slideInBottom 1.5s  ease-in-out",
+        "slide-in-left-100": "slideInLeft100 1.5s  ease-in-out",
+        "slide-in-right-100": "slideInRight100 1.5s  ease-in-out",
+        "fade-in": "fadeIn 1.5s ease-in-out",
+        typing: "typing 3s steps(40, end), blink .75s infinite alternate",
+      },
+      keyframes: {
+        slideInRight: {
+          from: { transform: "translateX(3%)", opacity: 0 },
+          to: { transform: "translateX(0)", opacity: 1 },
+        },
+        slideInLeft: {
+          from: { transform: "translateX(-3%)", opacity: 0 },
+          to: { transform: "translateX(0)", opacity: 1 },
+        },
+        slideInRight100: {
+          from: { transform: "translateX(80%)", opacity: 0 },
+          to: { transform: "translateX(0)", opacity: 1 },
+        },
+        slideInLeft100: {
+          from: { transform: "translateX(-80%)", opacity: 0 },
+          to: { transform: "translateX(0)", opacity: 1 },
+        },
+        slideInBottom: {
+          from: { transform: "translateY(3%)", opacity: 0 },
+          to: { transform: "translateY(0)", opacity: 1 },
+        },
+        fadeIn: {
+          from: { opacity: 0 },
+          to: { opacity: 1 },
+        },
+
+        typing: {
+          from: {
+            width: "0%",
+          },
+          to: {
+            width: "100%",
+          },
+        },
+        blink: {
+          from: {
+            "border-right-color": "transparent",
+          },
+          to: {
+            "border-right-color": "black",
+          },
         },
       },
     },
